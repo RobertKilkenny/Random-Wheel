dbea879c07ebb4f4b9e27de73750d11592bc98a0
Robert Kilkenny <robert.kilkenny7@gmail.com>
2024-09-17 16:21:49 -0400

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # RandomWheel
+By Robert Kilkenny
+
+## Description
  Making a simple website page that will spin a wheel automatically on load by having the elements sent via the URL
---
+++ Random-Wheel/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div id='Website Holder' className='site-holder'>
+        <div className='page-holder'>
+          Testing
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
